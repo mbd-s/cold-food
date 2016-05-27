fc6988f0b171c95d90e9801ecbcb12b52c9fb0e4
mbd-s <michaelbsilverberg@gmail.com>
2016-05-27 12:25:38 -0700

    can search twitter and tweet from command line

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-console.log('Cold Food Bot is running');
 require('dotenv').config();
+
 var Twit = require('twit');
 
 var config = require('./config')
-console.log(config);
+
 var T = new Twit(config);
+console.log('@ColdFoodBot is running on Twitter.');
+
+//sample search
+// T.get('search/tweets', { q: 'menus', lang: 'en', count: 10 }, function(err, data, response) {
+//   var tweets = data.statuses;
+//   for (var i = 0; i < tweets.length; i++) {
+//     console.log(tweets[i].text)
+//   }
+// });
+
+T.post('statuses/update', { status: 'The food is getting cold' }, function(err, data, response) {
+  if (err) {
+    console.log ("There was an error: ", err);
+  }
+  else {
+    console.log("Tweet tweeted");
+    console.log(data);
+  }
+});
