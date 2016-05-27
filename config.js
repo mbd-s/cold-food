3ad2cc52f7f9a4e6374b507dd77838fea2ce892c
mbd-s <michaelbsilverberg@gmail.com>
2016-05-27 09:55:21 -0700

    env for api tokens

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  consumer_key:         'YOUR_CONSUMER_KEY',
-  consumer_secret:      'YOUR_CONSUMER_SECRET',
-  access_token:         'YOUR_ACCESS_TOKEN',
-  access_token_secret:  'YOUR_ACCESS_TOKEN_SECRET'
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
+
+
+var client = new Twitter({
+  consumer_key: ,
+  consumer_secret: ,
+  access_token_key: ,
+  access_token_secret: ,
+});
