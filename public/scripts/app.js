74633ca21ecf137dc6f0d43cea0207d86bd15b36
mbd-s <michaelbsilverberg@gmail.com>
2016-05-31 17:53:09 -0700

    cleaned up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,17 +7,17 @@ function TweetsIndexController ( $http ) {
   var vm = this;
 
   $http({
-      method: 'GET',
-      url: '/tweets'
+    method: 'GET',
+    url: '/tweets'
     }).then(function successCallback(response) {
       vm.tweets = response.data;
     }, function errorCallback(response) {
       console.log('There was an error getting the data', response);
-    });
+  });
 
-    vm.editTweet = function (tweet) {
+  vm.editTweet = function (tweet) {
     $http({
-      method: 'PUT',
+      method: 'PATCH',
       url: '/tweets/' + tweet._id,
       data: tweet
     }).then(function successCallback(json) {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+//TODO add basic-auth
+
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
@@ -13,8 +15,6 @@ app.set('view engine', 'html');
 
 var controllers = require('./controllers');
 
-//TODO add basic auth
-
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -22,7 +22,7 @@ app.get('/', function homepage (req, res) {
 app.get('/tweets', controllers.tweets.index);
 app.get('/tweets/:tweetId', controllers.tweets.show);
 app.delete('/tweets/:tweetId', controllers.tweets.destroy);
-app.put('/tweets/:tweetId', controllers.tweets.update);
+app.patch('/tweets/:tweetId', controllers.tweets.update);
 
 app.get('/templates/:name', controllers.tweets.templates);
 
