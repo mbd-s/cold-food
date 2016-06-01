840f6e58fab3191a42f0257ecf3dc41823bfb53d
mbd-s <michaelbsilverberg@gmail.com>
2016-06-01 10:00:58 -0700

    updated tweet schema to default isReady-false

--- models/tweet.js
+++ models/tweet.js
@@ -3,9 +3,8 @@ var Schema = mongoose.Schema;
 
 var TweetSchema = new Schema({
      status: String,
-     isReady: Boolean
+     isReady: {type: Boolean, default: false}
 });
 
 var Tweet = mongoose.model('Tweet', TweetSchema);
-
 module.exports = Tweet;
