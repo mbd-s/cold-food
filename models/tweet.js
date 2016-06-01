abf03ee0b77f215583f83c4d94410b8e1ba063f5
mbd-s <michaelbsilverberg@gmail.com>
2016-06-01 14:58:54 -0700

    todo

--- models/tweet.js
+++ models/tweet.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TweetSchema = new Schema({
+    // TODO add validation for char length
      status: String,
      isReady: {type: Boolean, default: false}
 });
